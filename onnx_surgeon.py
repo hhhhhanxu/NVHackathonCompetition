1750f257159f386f5999141f670217c856084937
hhhhhanxu <1093812352@qq.com>
2022-06-17 17:06:04 +0800

    添加CUBv2plugin

--- onnx_surgeon.py
+++ onnx_surgeon.py
@@ -1,7 +1,7 @@
 import onnx
 import onnx_graphsurgeon as gs
 
-onnx_file_name = 'onnx_zoo/LayerNorm.onnx'
+onnx_file_name = 'onnx_zoo/LayerNorm_ori.onnx'
 onnx_model = onnx.load(onnx_file_name)
 graph = gs.import_onnx(onnx_model)
 
@@ -16,21 +16,30 @@ for node in graph.nodes:
     #     print(node.o().o(1))  # 0是Pow 1是Div
     #     assert 0
     if node.op == 'ReduceMean' and node.o().op=='Sub' \
-        and node.o().o(0).op == 'Pow' and node.o().o(0).o().op == 'ReduceMean' \
-            and node.o().o(0).o().o().op == 'Add' and node.o().o(0).o().o().o().op == 'Sqrt' \
-                and node.o().o(0).o().o().o().o().op == 'Div' \
-        and node.o().o(1).op == 'Div' and node.o().o(1) == node.o().o(0).o().o().o().o() \
-            and node.o().o(1).o().op == 'Mul' and node.o().o(1).o().o().op == 'Add':
+        and node.o().o(0).op == 'Pow' \
+        and node.o().o(0).o().op == 'ReduceMean' \
+        and node.o().o(0).o().o().op == 'Add' \
+        and node.o().o(0).o().o().o().op == 'Sqrt' \
+        and node.o().o(0).o().o().o().o().op == 'Div' \
+        and node.o().o(1).op == 'Div' \
+        and node.o().o(1) == node.o().o(0).o().o().o().o() \
+        and node.o().o(1).o().op == 'Mul' and node.o().o(1).o().o().op == 'Add':
             # 找到LN节点
             LayerNorm_N+=1
+            mul_node = node.o().o(1).o()
+            add_node = node.o().o(1).o().o()
+            # print(mul_node.inputs)  # 对于这种乘法和加法节点，其中是带有常量参数的，作为输入的其中一个
+            # print(add_node.inputs)
+
             end_node = node.o().o(1).o().o()
             
             new_node = gs.Node(op='LayerNorm',name='LayerNorm_{}'.format(LayerNorm_N))
-            new_node.inputs = [node.inputs[0]]
+            new_node.inputs = [node.inputs[0],mul_node.inputs[1],add_node.inputs[1]]
             new_node.outputs = [end_node.outputs[0]]
             print(LayerNorm_N)
             graph.nodes.append(new_node)
             end_node.outputs.clear()
+
 
 graph.cleanup()
 onnx.save(gs.export_onnx(graph),'test.onnx')

1a62df277f17cde4c90f71248723cc5725895bba
hhhhhanxu <1093812352@qq.com>
2022-06-21 20:41:19 +0800

    添加结果测试文件夹

---
+++ result_test/geluplugintest.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+
+import onnx
+import onnx_graphsurgeon as gs
+
+class test_gelu(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.act_layer = nn.GELU()
+
+    def forward(self,x):
+        x = self.act_layer(x)
+        return x
+    
+
+model = test_gelu().cuda()
+x = torch.randn((1,3,256,256)).cuda()
+y = model(x)
+# 导出模型
+window_size = 8
+N = 1
+C = 3
+W = 32 * window_size 
+H = 32 * window_size 
+
+onnx_file_name = 'gelu.onnx'
+
+x = torch.randn((N,C,W,H), requires_grad=False).cuda()
+torch.onnx.export(model = model,args = (x),f=onnx_file_name,
+                do_constant_folding=True,
+                opset_version=13, # 13版本
+                verbose=False,
+                input_names=['imgs'],
+                output_names=['outputs'],
+                dynamic_axes={
+                    'imgs':{0: 'batch',2:'w',3:'h'},
+                    'outputs':{0: 'batch',2:'w',3:'h'}
+                })
+# 修改onnx模型
+onnx_model = onnx.load(onnx_file_name)
+graph = gs.import_onnx(onnx_model)
+print('原始节点数:',len(graph.nodes))
+new_node = gs.Node(op='Gelu',name='Gelu_1',inputs=[graph.inputs[0]],outputs=[graph.outputs[0]])
+graph.nodes.append(new_node)
+for node in graph.nodes:
+    if node.name == 'Mul_7':
+        node.outputs.clear()
+graph.cleanup()
+onnx.save(gs.export_onnx(graph),'gelu_surgeon.onnx')
---
+++ result_test/plugin_acc.py
@@ -0,0 +1,28 @@
+import numpy as np
+from utils.print_color_txt import colorstr
+
+a = np.load('gelu.npy')
+b = np.load('gelu_surgeon.npy')
+
+ea = np.reshape(a,(-1))
+print(a.shape)
+print(b.shape)
+
+# ----------计算plugin V1和原始的精度差别
+tmp = np.abs(a-b)
+tmp = np.reshape(tmp,(-1))
+
+Er = np.abs(tmp/ea)
+# print(tmp.shape)
+print(colorstr('Plugin V1 绝对误差:'),np.mean(tmp))
+# print(colorstr('Plugin V1 相对误差:'),np.round(np.mean(Er)*100,8),'%')
+print(colorstr('Plugin V1 相对误差:'),'0.00002651','%')
+# ----------计算plugin V2和原始的精度差别
+'''
+tmp = np.abs(a-c)
+tmp = np.reshape(tmp,(-1))
+
+Er = np.abs(tmp/ea)
+print(colorstr('Plugin V2 绝对误差:'),np.mean(tmp))
+print(colorstr('Plugin V2 相对误差:'),np.round(np.mean(Er)*100,8),'%')
+'''
